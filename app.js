d2880f21cf6190a3a5092522ef9dc4844ee2a726
Brian Zhang <bzhang1670@gmail.com>
2016-11-12 00:50:00 -0800

    pushed skeleton site

---
+++ app.js
@@ -0,0 +1,23 @@
+var http = require('http');
+//var html = require('html');
+var express = require('express');
+var app = express();
+var path = require('path');
+
+var TEMPLATE_DIR =  __dirname + '/template/'
+
+app.get('/', function(req, res) {
+	app.set('views', __dirname + "/template");
+	app.use('/css', express.static(path.join(__dirname, 'public/css')));
+	console.log('Base name:' + __dirname);
+	res.sendFile(TEMPLATE_DIR + 'index.html');
+});
+
+app.get('/index', function(req, res) {
+	res.sendFile(path.join(TEMPLATE_DIR + 'index.html'));
+});
+
+
+app.listen(8080, function() {
+	console.log('Example app listening on port 8080');
+});
